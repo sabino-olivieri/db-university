1168557ac24b8955f57840377b4439b690dd3862
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-29 16:13:47 +0200

    feat: exercise completed

--- query.sql
+++ query.sql
@@ -24,3 +24,31 @@ SELECT *
 FROM `exams`
 WHERE HOUR(`hour`) >= 14  
 AND `date` = "2020-06-20";
+
+-- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = "magistrale";
+
+-- 7. Da quanti dipartimenti è composta l'università? (12)
+SELECT COUNT(`id`) AS `number_of_departments`
+FROM `departments`;
+
+-- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * 
+FROM `teachers`
+WHERE `phone` IS NOT null;
+
+-- 9. Inserire nella tabella degli studenti un nuovo record con i propri dati (per il campo degree_id, inserire un valore casuale)
+INSERT INTO `students` (`degree_id`, `name`, `surname`, `date_of_birth`, `fiscal_code`, `enrolment_date`, `registration_number`, `email`)
+VALUES ("20", "Sabino", "Olivieri", "1996-02-28", "LVRSBN96B28C983U", "2024-05-29", "625033", "olivierisabino@gmail.com");
+
+-- 10. Cambiare il numero dell’ufficio del professor Pietro Rizzo in 126
+UPDATE `teachers`
+SET `office_number` = "126"
+WHERE `name` = "Pietro" 
+AND `surname`= "Rizzo";
+
+-- 11. Eliminare dalla tabella studenti il record creato precedentemente al punto 9
+DELETE FROM `students`
+WHERE `fiscal_code` = "LVRSBN96B28C983U";
